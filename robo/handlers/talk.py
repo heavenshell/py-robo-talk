2d9c0cd27b51a953601583c4042e7354316845a8
heavenshell <heavenshell.jp@gmail.com>
2015-02-13 00:27:49 +0900

    Fix not work.

--- robo/handlers/talk.py
+++ robo/handlers/talk.py
@@ -16,14 +16,27 @@ import logging
 from doco.client import Client as DocomoClient
 from robo.decorators import cmd
 
+logger = logging.getLogger('robo')
+
 
 class Client(object):
     def __init__(self):
+        """Construct docomo client.
+
+        If `DOCOMO_API_KEY` does not set in ENV, raise exception.
+        """
         apikey = os.environ.get('DOCOMO_API_KEY', None)
+        if apikey is None:
+            raise Exception('DOCOMO_API_KEY is None')
+
         self.client = DocomoClient(apikey=apikey)
         self.options = None
 
     def talk(self, message):
+        """Talk with Docomo dialogue api.
+
+        :param message: Message
+        """
         if self.options is None:
             res = self.client.send(utt=message, apiname='Dialogue')
         else:
@@ -44,14 +57,17 @@ class Talk(object):
             self.options = options['doco']
 
     def __init__(self):
-        #: Change requests log level.
+        #: Change requests and doco's log level.
         logging.getLogger('requests').setLevel(logging.ERROR)
         logging.getLogger('doco').setLevel(logging.ERROR)
         self.client = Client()
-        self.options = None
 
     @cmd(regex=r'.+',
          missing=True,
          description='Talk if given message didn\'t match any other handlers')
     def get(self, message, **kwargs):
-        return self.client.talk(message.match.group(0))
+        try:
+            return self.client.talk(message.match.group(0))
+        except Exception as e:
+            logger.error('Message is {0}', message)
+            logger.error(e)
